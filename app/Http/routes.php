0331921a76973b9d65bfe9a4c4cc31b05c59c4ce
max <max@max.com>
2018-07-28 08:44:12 +0800

    cont post create

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,4 +26,5 @@ Route::get('/admin',function(){
 
 Route::Group(['middleware'=>'admin'],function(){
     Route::resource('admin/users','AdminUserController');
+    Route::resource('admin/posts','AdminPostController');
 });
